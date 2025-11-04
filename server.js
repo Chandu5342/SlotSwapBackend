8fc478c39ad609723ccfb06eb6e1022193a0818d
Chandu5342 <karrichandu309@gmail.com>
2025-11-04 21:01:35 +0530

    after middleware

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-
+import authRoutes from "./routes/authRoutes.js";    
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("backend is running")
 });
