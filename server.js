4fdfd2ee7f637255287743f23331dc3dd7ccbdc6
Chandu5342 <karrichandu309@gmail.com>
2025-11-04 19:55:37 +0530

    initial server.js setup

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("backend is running")
+});
+
+
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+});
